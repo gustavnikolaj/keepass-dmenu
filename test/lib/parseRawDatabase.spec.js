8ba4427c126d910c2c806375b3861dbfba4c2c1f
Gustav Nikolaj Olsen <gno@one.com>
2015-01-02 20:10:29 +0100

    document workaround for single entry

--- test/lib/parseRawDatabase.spec.js
+++ test/lib/parseRawDatabase.spec.js
@@ -6,13 +6,21 @@ var parseRawDatabase = require('../../lib/parseRawDatabase');
 
 function getTestFile(filename) {
     var testDataPath = require('path').resolve(__dirname, '..', '..', 'testdata', 'rawDatabaseInJsonFormat');
-    return fs.readFileSync(testDataPath + '/' + filename, 'utf-8');
+    var content = fs.readFileSync(testDataPath + '/' + filename, 'utf-8');
+    content = content.replace(/^\/\/.*?\n/gm, ''); // Strip comments
+    return content;
 }
 
 
 describe('lib/parseRawDatabase', function () {
     it('should be able to pass groups both singular and multiple', function () {
         var data = JSON.parse(getTestFile('groups.json'));
+        expect(function () {
+            parseRawDatabase(data);
+        }, 'not to throw');
+    });
+    it('should be able to pass entries both singular and multiple', function () {
+        var data = JSON.parse(getTestFile('entries.json'));
         expect(function () {
             parseRawDatabase(data);
         }, 'not to throw');
